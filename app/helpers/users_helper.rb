aa228e7b7dd16805fbeb7e4ccd3ee9881c1802af
Smai Fullerton <smai.fullerton@nationbuilder.com>
2021-07-28 17:08:44 -0600

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  # Returns the gravatar for a given user
+  def gravatar_for(user, size: 80)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '', email: 'user@invalid', password: '', password_confirmation: '' } }
+    end
+    assert_template 'users/new'
+    assert_select '.sign_up_form li' do |lis|
+      error_messages = lis.map(&:to_s)
+
+      # Not actually a good idea to verify underlying Rails messages, just trying stuff.
+      expected_errors = ["Name can't be blank", 'Email is invalid', "Password can't be blank", 'Password is too short']
+      expected_errors.each do |m|
+        assert error_messages.any? { |msg|
+                 msg.include?(m)
+               }, "Expected error message #{m} not present"
+      end
+    end
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path,
+           params: { user: { name: 'Example User', email: 'email@valid.com', password: 'test123',
+                             password_confirmation: 'test123' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
