5f72c3e1bbaf69130d4da8364cdf1b6aa1717856
Smai Fullerton <smai.fullerton@nationbuilder.com>
2021-08-02 09:47:19 -0600

    Delete microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,11 +13,27 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+
+    # Edge case where user posts an invalid micropost
+    # and then tries to delete a post from home page
+    if request.referrer == microposts_url
+      redirect_to root_url
+    else
+      redirect_back(fallback_location: root_url)
+    end
+  end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
